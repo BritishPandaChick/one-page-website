5f73a57b2fbfb11fcab36ec92cc0d57bf53f10f2
BritishPandaChick <sbartley1080@gmail.com>
2019-01-04 00:05:01 -0500

    Update WordPress to latest version. Update plugins

--- wp-content/uploads/sucuri/sucuri-failedlogins.php
+++ wp-content/uploads/sucuri/sucuri-failedlogins.php
@@ -1,3 +1,1 @@
 <?php exit(0); ?>
-{"user_login":"jeenaChoi4080","user_password":"","attempt_time":1518570636,"remote_addr":"127.0.0.1","user_agent":"Mozilla\/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/64.0.3282.140 Safari\/537.36"}
-{"user_login":"jeenaChoi4080","user_password":"","attempt_time":1518570657,"remote_addr":"127.0.0.1","user_agent":"Mozilla\/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/64.0.3282.140 Safari\/537.36"}
--- wp-content/uploads/sucuri/sucuri-hookdata.php
+++ wp-content/uploads/sucuri/sucuri-hookdata.php
@@ -1,6 +1,6 @@
 <?php
 // datastore=hookdata;
 // created_on=1540681698;
-// updated_on=1540681698;
+// updated_on=1546577600;
 exit(0);
 ?>
